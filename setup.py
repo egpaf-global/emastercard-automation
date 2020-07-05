9c2b8e755283d60a0c1db0c8fc967b6cf6a07885
Walter Kaunda <walterkaunda@live.co.uk>
2020-07-05 15:08:15 +0200

    Added forced tag fetch on every online installation.
    - This is to handle situations where a tag is force updated.

--- setup.py
+++ setup.py
@@ -26,8 +26,9 @@ def run(command, die_on_fail=True):
 def update_repo(repository, branch='master', tag=None):
     '''Clones or updates a repository.'''
     def get_tag():
+        run('git fetch --tags -f')
+        
         if tag is None:
-            run('git fetch --tags -f')
             return '`git describe --tags`'
             
         return tag
