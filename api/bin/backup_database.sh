037a32c01ebc63e279bdc52ce9856640dc20619b
Roy Chanunkha <roychanunkha@gmail.com>
2024-04-18 12:42:36 +0200

    chore: updating bash scripts

--- api/bin/backup_database.sh
+++ api/bin/backup_database.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
-USERNAME=`ruby -ryaml -e "puts YAML::load_file('/opt/BHT-EMR-API/config/database.yml')['development']['username']"`
-PASSWORD=`ruby -ryaml -e "puts YAML::load_file('/opt/BHT-EMR-API/config/database.yml')['development']['password']"`
-DATABASE=`ruby -ryaml -e "puts YAML::load_file('/opt/BHT-EMR-API/config/database.yml')['development']['database']"`
-HOST=`ruby -ryaml -e "puts YAML::load_file('/opt/BHT-EMR-API/config/database.yml')['development']['host']"`
+USERNAME=`ruby -ryaml -e "puts YAML.safe_load(File.read('/opt/BHT-EMR-API/config/database.yml'), aliases: true)['${ENV}']['username']"`
+PASSWORD=`ruby -ryaml -e "puts YAML.safe_load(File.read('/opt/BHT-EMR-API/config/database.yml'), aliases: true)['${ENV}']['password']"`
+DATABASE=`ruby -ryaml -e "puts YAML.safe_load(File.read('/opt/BHT-EMR-API/config/database.yml'), aliases: true)['${ENV}']['database']"`
+HOST=`ruby -ryaml -e "puts YAML.safe_load(File.read('/opt/BHT-EMR-API/config/database.yml'), aliases: true)['${ENV}']['host']"`
 
 mysqldump --user=$USERNAME --password=$PASSWORD --host=$HOST $DATABASE
