aec782f77695049f9121758139b10d1af22c0b5f
mnaboti <mnaboti@pedaids.org>
2024-07-10 16:57:39 +0200

    v2024.Q2.R6

--- web/static/assets/datatable-6c42f8c8.js
+++
@@ -1,1 +0,0 @@
-import{d9 as n,g as l}from"./index-4d483828.js";function u(t="person_id",r=l()){return{label:"view client",icon:n,color:"light",action:e=>r.push("/emc/patient/".concat(e[t]))}}function i(t,r={}){return Array.isArray(t)?t.map(e=>({label:e,path:e,drillable:!0,sortable:!0,...r})):Object.entries(t).map(([e,a])=>({path:e,label:a,drillable:!0,sortable:!1,...r}))}export{u as g,i as t};
