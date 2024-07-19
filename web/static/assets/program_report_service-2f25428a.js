becb3dfa74372895f61fbd260fb1d193deeb0af2
Daniel Justin <danieljmuleke@gmail.com>
2024-07-19 13:23:04 +0200

    v2024.Q2.R8

--- web/static/assets/program_report_service-2f25428a.js
+++
@@ -1,1 +0,0 @@
-import{R as t}from"./report_service-e6bbf7f9.js";class p extends t{constructor(){super()}generate(r,e){return this.getReport("/programs/".concat(this.programId,"/reports/").concat(r),e)}}export{p as P};
